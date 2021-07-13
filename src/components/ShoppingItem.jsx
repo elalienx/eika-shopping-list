6ff9ed86eaf82bd29be176d9730598ab24c412de
Eduardo Alvarez <el_alienx@hotmail.com>
2021-07-13 10:47:58 +0200

    made the code shorter by inlining the method call markAsComplete on Shopping Item

--- src/components/ShoppingItem.jsx
+++ src/components/ShoppingItem.jsx
@@ -2,20 +2,13 @@ export default function ShoppingItem({ item, markAsComplete }) {
   // Properties
   const { id, name, price, isCompleted } = item;
 
-  // Methods
-  function onChange() {
-    console.log(`Item clicked #${id}`);
-    markAsComplete(id);
-  }
-
-  /**
-   * The next challenge...
-   * how do we move this id back to App.jsx to update the list
-   */
-
   return (
     <article>
-      <input checked={isCompleted} onChange={onChange} type="checkbox" />
+      <input
+        checked={isCompleted}
+        onChange={() => markAsComplete(id)}
+        type="checkbox"
+      />
       <label>{name}</label>
       <span>{price}</span>
     </article>
