18f6d12351087870e7a908484f4193ee338ed9de
Eduardo Alvarez <el_alienx@hotmail.com>
2021-07-13 15:08:22 +0200

    added the filtered list

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ export default function App() {
   // Local state
   const [list, setList] = useState(DummyData);
 
+  const activeItems = list.filter((item) => item.isCompleted === false);
+  const inactiveItems = list.filter((item) => item.isCompleted === true);
+
   // Methods
   function oncChange(id) {
     const item = list[id];
@@ -26,10 +29,13 @@ export default function App() {
 
       {list.length === 0 && <EmptyState />}
 
-      {/* Completed */}
-      {list.length > 0 && <ShoppingList list={list} oncChange={oncChange} />}
+      <h2>Pending</h2>
+      {list.length > 0 && (
+        <ShoppingList list={activeItems} oncChange={oncChange} />
+      )}
 
-      {/* Archived */}
+      <h2>Completed</h2>
+      <ShoppingList list={inactiveItems} oncChange={oncChange} />
     </div>
   );
 }
