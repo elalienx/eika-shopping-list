606b055182953c5e709507de3871c4bbc4778ffa
Eduardo Alvarez <el_alienx@hotmail.com>
2021-07-13 10:36:39 +0200

    refactor the dummy data into a separate json file

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@
 import { useState } from "react";
 
 // Local imports
+import DummyData from "./dummyData.json";
 import EmptyState from "./components/EmptyState";
 import ShoppingList from "./components/ShoppingList";
 
 export default function App() {
-  // scratch data
-  const item1 = { name: "Voxlöv chair", price: 795, isCompleted: false };
-  const item2 = { name: "Kipplan sofa", price: 1995, isCompleted: false };
-  const item3 = { name: "Morgedal matress", price: 1495, isCompleted: false };
-  const item4 = { name: "Bed sheets", price: 79, isCompleted: true };
-
   // Local state
-  const [list, setList] = useState([item1, item2, item3, item4]);
+  const [list, setList] = useState(DummyData);
 
   return (
     <div className="App">
--- src/components/ShoppingItem.jsx
+++ src/components/ShoppingItem.jsx
@@ -2,9 +2,14 @@ export default function ShoppingItem({ item }) {
   // Properties
   const { name, price, isCompleted } = item;
 
+  // Methods
+  function onChange() {
+    alert("foo");
+  }
+
   return (
     <article>
-      <input type="checkbox" checked={isCompleted} />
+      <input type="checkbox" checked={isCompleted} onChange={onChange} />
       <label>{name}</label>
       <span>{price}</span>
     </article>
