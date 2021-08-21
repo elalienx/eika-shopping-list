3d1e6ae3d47e5b52806e63bea1779e15d4fa0c69
Eduardo Alvarez <el_alienx@hotmail.com>
2021-08-21 15:06:10 +0200

    passed all test

--- src/components/Sorter.test.jsx
+++ src/components/Sorter.test.jsx
@@ -2,10 +2,14 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 
 // Project files
-import { ListProvider } from "../state/ListProvider";
 import { sortByString, sortByNumber } from "../scripts/sorter/list-sorter";
+import { ListProvider } from "../state/ListProvider";
 import Sorter from "./Sorter";
 jest.mock("../scripts/sorter/list-sorter");
+
+beforeEach(() => {
+  window.localStorage.clear();
+});
 
 test("Should call the sortByString function", async () => {
   // Arrange
@@ -24,7 +28,7 @@ test("Should call the sortByString function", async () => {
   expect(sortByString).toHaveBeenCalledTimes(1); // to verify if our React component call this external module method
 });
 
-test("Should cal the sortByNumber function", async () => {
+test("Should call the sortByNumber function", async () => {
   // Arrange
   render(
     <ListProvider>
